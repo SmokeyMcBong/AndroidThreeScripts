f125286f16ce924a40481545febebe014803f422
SmokeyMcBong <blackthesoul@gmail.com>
2014-11-26 16:18:26 +0000

    Added Proper License info

--- AndroidThreeScripts.sh
+++ AndroidThreeScripts.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#	This file is part of the 'AndroidThreeScripts' project  				#						
+#	Copyright (C) 2014  'theFONZ'											#
+#																			#
+#    This program is free software: you can redistribute it and/or modify	#
+#    it under the terms of the GNU General Public License as published by	#
+#    the Free Software Foundation, either version 2 of the License, or		#
+#    any later version.														#
+#																			#
+#	This program is distributed in the hope that it will be useful,  		#
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of  		#
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  					#
+#	See the GNU General Public License for more details.  					#
+#																			#
+#	You should have received a copy of the GNU General Public License		#
+#	along with this program.  If not, see COPYING.txt in project root       #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 source A3S-ui.cfg			# import Default Menu UI Settings
 source A3S.cfg				# import Default Script Settings
 source user.preferences		# import User Configurable Settings
